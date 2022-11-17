c5e1a466cc514ad9da74c22758e87f91d85b97f8
hawltu yenealem <yenealemhawltu@gmail.com>
2022-11-17 10:56:40 +0300

    updating item repository and sevice by adding either return type

--- lib/data/repository/item_repository.dart
+++ lib/data/repository/item_repository.dart
@@ -1,12 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:sample_firebase_project/data/models/error_response.dart';
 import 'package:sample_firebase_project/data/models/items_model.dart';
 import 'package:sample_firebase_project/data/services/item_service.dart';
 import 'package:sample_firebase_project/presentation/item/blocs/item_bloc.dart';
 
 abstract class ItemRepository {
-  Future<void> AddItem({required ItemModel itemModel});
-  Future<void> UpdateItem({required String name,required ItemModel itemModel});
-  Future<void> DeleteItem({required String name});
-  Future<List<ItemModel>> getItems();
+  Future<Either<ErrorResponse,bool>> AddItem({required ItemModel itemModel});
+  Future<Either<ErrorResponse,bool>> UpdateItem({required String name,required ItemModel itemModel});
+  Future<Either<ErrorResponse,bool>> DeleteItem({required String name});
+  Future<Either<ErrorResponse,List<ItemModel>>> getItems();
 
 }
 
@@ -16,25 +18,25 @@ class ItemRepositoryImpl extends ItemRepository{
   ItemRepositoryImpl ({required this.itemService});
 
   @override
-  Future<void> AddItem({required ItemModel itemModel}) async{
+  Future<Either<ErrorResponse,bool>> AddItem({required ItemModel itemModel}) async{
     // TODO: implement AddItem
-    await itemService.addItem(itemModel);
+    return await itemService.addItem(itemModel);
   }
 
   @override
-  Future<void> DeleteItem({required String name}) async{
+  Future<Either<ErrorResponse,bool>> DeleteItem({required String name}) async{
     // TODO: implement DeleteItem
-    await itemService.deleteItem(name);
+    return await itemService.deleteItem(name);
   }
 
   @override
-  Future<void> UpdateItem({required String name, required ItemModel itemModel}) async{
+  Future<Either<ErrorResponse,bool>> UpdateItem({required String name, required ItemModel itemModel}) async{
     // TODO: implement UpdateItem
-    await itemService.updateItem(name, itemModel);
+   return await itemService.updateItem(name, itemModel);
   }
 
   @override
-  Future<List<ItemModel>> getItems()async {
+  Future<Either<ErrorResponse,List<ItemModel>>> getItems()async {
     // TODO: implement getItems
     return await itemService.getItems();
   }
--- lib/data/services/item_service.dart
+++ lib/data/services/item_service.dart
@@ -1,8 +1,10 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sample_firebase_project/data/models/error_response.dart';
 import 'package:sample_firebase_project/data/models/items_model.dart';
 import 'package:sample_firebase_project/data/models/login_request.dart';
 
@@ -10,37 +12,39 @@ class ItemService {
 
   final db = FirebaseFirestore.instance;
 
- Future<void> addItem(ItemModel itemModel)async{
+ Future<Either<ErrorResponse,bool>> addItem(ItemModel itemModel)async{
     try{
-    db.collection('items').doc().set(itemModel.toJson()) ;
-
+    var response = db.collection('items').doc().set(itemModel.toJson()) ;
+    return Right(true);
     }catch(e){
-      throw Exception();
+      return Left(ErrorResponse(message: e.toString(),code: 404));
     }
 
   }
-  Future<List<ItemModel>> getItems()async{
+  Future<Either<ErrorResponse,List<ItemModel>>> getItems()async{
     try{
       var items = db.collection("items").doc().get() as List<ItemModel>;
-      return items;
+      return Right(items);
     }catch(e){
-      throw Exception();
+      return Left(ErrorResponse(message: e.toString(),code: 404));
     }
   }
 
-  Future<void> deleteItem(String name)async{
+  Future<Either<ErrorResponse,bool>> deleteItem(String name)async{
    try{
      db.collection('items').doc(name).delete();
+     return Right(true);
    }catch(e){
-     throw Exception();
+     return Left(ErrorResponse(message: e.toString(),code: 404));
    }
   }
 
-  Future<void> updateItem(String name,ItemModel itemModel)async{
+  Future<Either<ErrorResponse,bool>> updateItem(String name,ItemModel itemModel)async{
     try{
     db.collection('items').doc(name).update(itemModel.toJson());
+    return Right(true);
     }catch(e){
-      throw Exception();
+      return Left(ErrorResponse(message: e.toString(),code: 404));
     }
   }
 }
